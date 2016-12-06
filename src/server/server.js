1b2a0e521845b4991a5f79d19e0a93b4760b687d
Nim G <nimrodgrinvald+github@gmail.com>
2016-12-05 16:15:40 -0800

    Complete 'build' and 'test'  tasks in Gruntfile; correct package.json from 'thenewscope' to 'anewscope'

--- src/server/server.js
+++ src/server/server.js
@@ -5,12 +5,14 @@ let app = express();
 let port = 3300; // TODO: add an || with process.env.PORT configuration and switch order with 3000 when complete
 
 app.listen(port);
+// eslint-disable-next-line
 console.log('listening on port ', port);
 
 // middleware
   /*  may need to change so that only specific directories are served static,
       since some Angular-related files may require a different routing method */
-app.use(express.static('client'));
+// is it src/client or client? seems like 'client' breaks the build
+app.use(express.static('src/client'));
 
 //routing behavior that we will want to replace with Angular ui-route
 // insert any needed routing behavior here
