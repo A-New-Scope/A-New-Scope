668bf8e8afff16395f03024e6d41a43c70d42a84
Isaac <Isaac@Isaacs-MacBook-Pro.local>
2016-12-07 12:14:59 -0800

    undo lint

--- src/client/assets/scripts/auth.js
+++ src/client/assets/scripts/auth.js
@@ -1,26 +1,37 @@
 angular.module('authModule', [])
-.controller('authController', function($scope, $http, $state){
-  $scope.login = function(user, pass) { //MOVE TO FACTORY LATER
-    $http({
-      method: 'POST',
-      url: '/login',
-      data: {
-        username: user,
-        password: pass
-      }
-    });
-    $state.go('demo');
-  };
 
-  $scope.signup = function(user, pass) {
+.controller('authController', function($scope, $http, $state){
+  $scope.login = function(user, pass){ //MOVE TO FACTORY LATER
     $http({
-      method: 'POST',
-      url: '/signup',
+      method: "POST",
+      url: "/login",
       data: {
         username: user,
         password: pass
       }
-    });
-    //go somewhere do something
-  };
-});
+    }).then(function(res){
+      if(res){
+        $state.go('user')
+      }
+    })
+    $scope.user = null
+    $scope.pass = null
+  }
+
+  $scope.signup = function(user, pass){
+    $http({
+      method: "POST",
+      url: "/signup",
+      data: {
+        username: user,
+        password: pass
+      }
+    }).then(function(res){
+      if(res){
+        $state.go('login')
+      }
+    })
+    $scope.user = null
+    $scope.pass = null
+  }
+})
